996349689a93b306a2e20b6cfe9d12412b177a63
Ben Thomasson <ben.thomasson@gmail.com>
2025-05-20 10:20:30 -0400

    Add basic prompts

---
+++ test_1.py
@@ -0,0 +1,37 @@
+from util import generate_response, parse_code_blobs, parse_json_blocks
+from local_python_executor import LocalPythonInterpreter
+
+
+def test_hello_world():
+    interp = LocalPythonInterpreter([], {})
+    response = generate_response(
+        "deepseek-r1-14b",
+        "helpful_code_assistant",
+        "hello_world",
+        cache_prompt=True,
+    )
+    print(response)
+    code = parse_code_blobs(response)
+    assert code is not None, "No code found"
+    print(code)
+    output, execution_logs, is_final_answer, trace = interp(code, {})
+    print(output)
+    print(execution_logs)
+    print(is_final_answer)
+    print(trace)
+    assert "hello" in execution_logs.lower()
+    assert "world" in execution_logs.lower()
+
+
+def test_json_block():
+    response = generate_response(
+        "deepseek-r1-14b",
+        "helpful_assistant",
+        "count_json",
+        cache_prompt=True,
+    )
+    print(response)
+    count = parse_json_blocks(response)
+    print(count)
+    assert count is not None
+    assert count[0] == [1,2,3,4,5,6,7,8,9,10]
---
+++ util.py
@@ -0,0 +1,177 @@
+import ast
+import json
+import logging
+import os
+import re
+from json.decoder import JSONDecodeError
+from uuid import uuid4
+
+import yaml
+from litellm import completion
+
+logger = logging.getLogger(__name__)
+
+FULL_MODEL_NAMES = {
+    'deepseek-r1-14b': 'ollama/deepseek-r1:14b',
+}
+
+
+def load_prompt(prompt_file_name, replacements=None):
+    with open(os.path.join("prompts", prompt_file_name)) as f:
+        prompt = f.read()
+    if replacements:
+        for key, value in replacements.items():
+            key = "{{%s}}".format(key)
+            prompt = prompt.replace(key, value)
+    logger.info("Prompt loaded: %s", prompt)
+    return prompt
+
+
+def generate_response(
+    model_name,
+    system_prompt_file_name,
+    user_prompt_file_name,
+    replacements=None,
+    **parameters_dict,
+):
+    user_prompt_text = load_prompt(user_prompt_file_name, replacements=replacements)
+    system_prompt_text = load_prompt(system_prompt_file_name, replacements=replacements)
+    response = completion(
+        model=FULL_MODEL_NAMES[model_name],
+        messages=[
+            {"role": "system", "content": system_prompt_text},
+            {"role": "user", "content": user_prompt_text},
+        ],
+        **parameters_dict,
+    )
+    os.makedirs(
+        os.path.join("responses", model_name, system_prompt_file_name, user_prompt_file_name),
+        exist_ok=True,
+    )
+    result_text = response["choices"][0]["message"]["content"]
+    response_output_file = os.path.join(
+        "responses",
+        model_name,
+        system_prompt_file_name,
+        user_prompt_file_name,
+        f"{uuid4()}.txt",
+    )
+    print(response_output_file)
+    with open(
+        response_output_file,
+        "w",
+    ) as f:
+        f.write(result_text)
+    return result_text
+
+
+def find_responses(model_name, system_prompt_file_name, user_prompt_file_name):
+    responses = []
+    responses_dir = os.path.join(
+        "responses", model_name, system_prompt_file_name, user_prompt_file_name
+    )
+    if os.path.exists(responses_dir):
+        for response_file_name in os.listdir(
+            os.path.join("responses", model_name, system_prompt_file_name, user_prompt_file_name)
+        ):
+            with open(
+                os.path.join(
+                    "responses",
+                    model_name,
+                    system_prompt_file_name,
+                    user_prompt_file_name,
+                    response_file_name,
+                )
+            ) as f:
+                responses.append(response_file_name)
+    return responses
+
+
+def load_responses(model_name, system_prompt_file_name, user_prompt_file_name):
+    responses = []
+    responses_dir = os.path.join(
+        "responses", model_name, system_prompt_file_name, user_prompt_file_name
+    )
+    if os.path.exists(responses_dir):
+        for response_file_name in os.listdir(
+            os.path.join("responses", model_name, system_prompt_file_name, user_prompt_file_name)
+        ):
+            with open(
+                os.path.join(
+                    "responses",
+                    model_name,
+                    system_prompt_file_name,
+                    user_prompt_file_name,
+                    response_file_name,
+                )
+            ) as f:
+                responses.append((f.read(), response_file_name))
+    return responses
+
+
+def load_response(model_name, system_prompt_file_name, user_prompt_file_name, response_file_name):
+    with open(
+        os.path.join(
+            "responses",
+            model_name,
+            system_prompt_file_name,
+            user_prompt_file_name,
+            response_file_name,
+        )
+    ) as f:
+        return f.read()
+
+
+# based on smolagents/src/smolagents/util.py
+def parse_code_blobs(code_blob: str) -> str:
+    """Parses the LLM's output to get any code blob inside. Will return the
+    code directly if it's code."""
+    pattern = r"```(?:py|python)\n(.*?)\n\s*```"
+    matches = re.findall(pattern, code_blob, re.DOTALL)
+    if len(matches) == 0:
+        try:  # Maybe the LLM outputted a code blob directly
+            ast.parse(code_blob)
+            return code_blob
+        except SyntaxError as e:
+            print(e)
+        return None
+
+    # Join all the code blobs together
+    return "\n\n".join(match.strip() for match in matches)
+
+
+def parse_json_blocks(json_block: str) -> str:
+    pattern = r"```(?:json)\n(.*?)\n\s*```"
+    matches = re.findall(pattern, json_block, re.DOTALL)
+    if len(matches) == 0:
+        try:  # Is the whole output a JSON block?
+            return [json.loads(json_block)]
+        except JSONDecodeError:
+            pass
+        return None
+
+    return [json.loads(match) for match in matches]
+
+
+def parse_yaml_blocks(yaml_block: str) -> str:
+    pattern = r"```(?:yaml)\n(.*?)\n\s*```"
+    matches = re.findall(pattern, yaml_block, re.DOTALL)
+    return [yaml.safe_load(match) for match in matches]
+
+
+def parse_blocks(block: str) -> str:
+    pattern = r"```\n(.*?)\n\s*```"
+    matches = re.findall(pattern, block, re.DOTALL)
+    return matches
+
+
+def parse_thinking_blocks(block: str) -> str:
+    pattern = r"<think>\n(.*?)\n\s*</think>"
+    matches = re.findall(pattern, block, re.DOTALL)
+    return matches
+
+
+def parse_answer_blocks(block: str) -> str:
+    pattern = r"<think>\n(?:.*?)\n\s*</think>\n(.*)"
+    matches = re.findall(pattern, block, re.DOTALL)
+    return matches
